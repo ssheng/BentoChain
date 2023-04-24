f21ee24df7e29162d3a9c2793334dc1ba19fdfb4
Ahmed Besbes <ahmed.besbes.esmoz@servier.com>
2023-04-24 11:49:57 +0200

    Fix route s2t to accept np array as input

--- speech2text_runner.py
+++ speech2text_runner.py
@@ -16,21 +16,9 @@ class Speech2TextRunnable(bentoml.Runnable):
         self.model = bentoml.transformers.load_model(s2t_model_ref)
 
     @bentoml.Runnable.method(batchable=False)
-    def transcribe_audio(self, audio_path):
-        if audio_path is not None:
-            audio_dataset = Dataset.from_dict({"audio": [audio_path]}).cast_column(
-                "audio",
-                Audio(sampling_rate=16000),
-            )
-            sample = audio_dataset[0]["audio"]
-
-            input_features = self.processor(
-                sample["array"],
-                sampling_rate=sample["sampling_rate"],
-                return_tensors="pt",
-            ).input_features
-
-            predicted_ids = self.model.generate(input_features)
+    def transcribe_audio(self, tensor):
+        if tensor is not None:
+            predicted_ids = self.model.generate(tensor)
             transcriptions = self.processor.batch_decode(
                 predicted_ids, skip_special_tokens=True
             )
